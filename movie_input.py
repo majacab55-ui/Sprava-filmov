c49c064b95c44de5ec97ced383bf432020fec298
majac <majacab55@gmail.com>
2026-03-26 20:52:13 +0100

    test

--- movie_input.py
+++ movie_input.py
@@ -1,9 +1,48 @@
 class Movie:
     def movie_input(self):
-        self.name = input("Zadaj meno filmu: ")
-        self.genre = input("Zadaj žáner: ")
-        self.year = int(input("Zadaj rok: "))
-        self.rating = int(input("Zadaj hodnotenie: "))
+        while True:
+            name = input("Zadaj meno filmu: ")
+            if name.strip() == "":
+                print("Chyba! Meno nemôže byť prázdne.")
+            elif name.isdigit():
+                print("Chyba! Meno nemôže byť iba číslo.")
+            else:
+                self.name = name
+                break
+        while True:
+            genre = input("Zadaj žáner: ")
+            if genre.strip() == "":
+                print("Chyba! Žáner nemôže byť prázdny.")
+            elif genre.isdigit():
+                print("Chyba! Žáner nemôže byť číslo.")
+            else:
+                self.genre = genre
+                break
+        while True:
+            try:
+                year = int(input("Zadaj rok vydania filmu: "))
+                if year < 1895: # rok prvého filmu
+                    print("Chyba! Film ešte neexistoval.")
+                elif year > 2026:
+                    print("Chyba! Rok nemôže byť v budúcnosti.")
+                elif len(str(year)) != 4:
+                    print("Chyba! Rok musí mať presne 4 číslice.")
+                else:
+                    self.year = year
+                    break
+
+            except ValueError:
+                print("Chyba! Zadaj číslo.")
+        while True:
+            try:
+                rating = float(input("Zadaj hodnotenie (0-10): "))
+                if 0 <= rating <= 10:
+                    self.rating = rating
+                    break
+                else:
+                    print("Chyba! Hodnotenie musí byť od 0 do 10.")
+            except ValueError:
+                print("Chyba! Zadaj číslo.")
 
     def show_info(self):
         print(self.name, self.genre, self.year, self.rating)
@@ -17,7 +56,7 @@ while True:
         break
 
     movie = Movie()
-    movie.Movie_input()
+    movie.movie_input()
     movies.append(movie)
 
 print("\nZoznam filmov:")
