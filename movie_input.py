250dca764a5a2da98e2f7631c5dc4149be29440e
majac <majacab55@gmail.com>
2026-03-28 23:13:05 +0100

    test

--- movie_input.py
+++ movie_input.py
@@ -1,51 +1,54 @@
 class Movie:
-    def movie_input(self):
-        while True:
-            name = input("Zadaj meno filmu: ")
-            if name.strip() == "":
-                print("Chyba! Meno nemôže byť prázdne.")
-            elif name.isdigit():
-                print("Chyba! Meno nemôže byť iba číslo.")
-            else:
-                self.name = name
-                break
-        while True:
-            genre = input("Zadaj žáner: ")
-            if genre.strip() == "":
-                print("Chyba! Žáner nemôže byť prázdny.")
-            elif genre.isdigit():
-                print("Chyba! Žáner nemôže byť číslo.")
-            else:
-                self.genre = genre
-                break
-        while True:
-            try:
-                year = int(input("Zadaj rok vydania filmu: "))
-                if year < 1895: # rok prvého filmu
-                    print("Chyba! Film ešte neexistoval.")
-                elif year > 2026:
-                    print("Chyba! Rok nemôže byť v budúcnosti.")
-                elif len(str(year)) != 4:
-                    print("Chyba! Rok musí mať presne 4 číslice.")
-                else:
-                    self.year = year
-                    break
-
-            except ValueError:
-                print("Chyba! Zadaj číslo.")
-        while True:
-            try:
-                rating = float(input("Zadaj hodnotenie (0-10): "))
-                if 0 <= rating <= 10:
-                    self.rating = rating
-                    break
-                else:
-                    print("Chyba! Hodnotenie musí byť od 0 do 10.")
-            except ValueError:
-                print("Chyba! Zadaj číslo.")
+    def __init__(self, name, genre, year, rating):
+            self.name = name
+            self.genre = genre
+            self.year = year
+            self.rating = rating
 
     def show_info(self):
         print(self.name, self.genre, self.year, self.rating)
+
+def movie_input():
+    while True:
+        name = input("Zadaj meno filmu: ")
+        if name.strip() == "":
+            print("Chyba! Meno nemôže byť prázdne.")
+        elif name.isdigit():
+            print("Chyba! Meno nemôže byť iba číslo.")
+        else:
+            break
+    while True:
+        genre = input("Zadaj žáner: ")
+        if genre.strip() == "":
+            print("Chyba! Žáner nemôže byť prázdny.")
+        elif genre.isdigit():
+            print("Chyba! Žáner nemôže byť číslo.")
+        else:
+            break
+    while True:
+        try:
+            year = int(input("Zadaj rok vydania filmu: "))
+            if year < 1895: # rok prvého filmu
+                print("Chyba! Film ešte neexistoval.")
+            elif year > 2026:
+                print("Chyba! Rok nemôže byť v budúcnosti.")
+            elif len(str(year)) != 4:
+                print("Chyba! Rok musí mať presne 4 číslice.")
+            else:
+                break
+        except ValueError:
+            print("Chyba! Zadaj číslo.")
+    while True:
+        try:
+            rating = float(input("Zadaj hodnotenie (0-10): "))
+            if 0 <= rating <= 10:
+                break
+            else:
+                print("Chyba! Hodnotenie musí byť od 0 do 10.")
+        except ValueError:
+            print("Chyba! Zadaj číslo.")
+
+    return Movie(name, genre, year, rating)
 
 movies = []
 
@@ -55,8 +58,7 @@ while True:
     if stop == "stop":
         break
 
-    movie = Movie()
-    movie.movie_input()
+    movie = movie_input()
     movies.append(movie)
 
 print("\nZoznam filmov:")
