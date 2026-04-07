746d637d1e36cd55c6433a792e9fe8498fec3de9
majac <majacab55@gmail.com>
2026-04-07 06:54:34 +0200

    Api

--- movie_input.py
+++ movie_input.py
@@ -1,3 +1,5 @@
+import requests
+
 class Movie:
     def __init__(self, name, genre, year, rating):
             self.name = name
@@ -6,7 +8,11 @@ class Movie:
             self.rating = rating
 
     def show_info(self):
-        print(self.name, self.genre, self.year, self.rating)
+        print(f"Názov: {self.name}")
+        print(f"Žáner: {self.genre}")
+        print(f"Rok: {self.year}")
+        print(f"Hodnotenie: {self.rating}")
+        print("------------------")
 
 def movie_input():
     while True:
@@ -50,17 +56,73 @@ def movie_input():
 
     return Movie(name, genre, year, rating)
 
+def get_movie_from_api(name):
+    api_key = "6094012a" # osobny key
+    url = f"http://www.omdbapi.com/?t={name}&apikey={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if data["Response"] == "True":
+        try:
+            year = int(data["Year"][:4])
+        except:
+            year = 0
+        try:
+            rating = float(data["imdbRating"]) if data["imdbRating"] != "N/A" else 0
+        except:
+            rating = 0
+
+        return Movie(data["Title"], data["Genre"], year, rating)
+    else:
+        return None
+
+def find_movie(movies):
+    search = input("Zadaj názov filmu: ")
+
+    for m in movies:
+        if search.lower() in m.name.lower():
+            print("Nájdené:")
+            m.show_info()
+            return
+
+    print("Film sa nenašiel.")
+
+
 movies = []
 
 while True:
-    stop = input("Napíš 'stop' ak chceš skončiť, inak ENTER: ")
+    print("\n1 - Pridať film ručne")
+    print("2 - Pridať film z API")
+    print("3 - Vyhľadať film")
+    print("4 - Zobraziť všetky filmy")
+    print("5 - Koniec")
 
-    if stop == "stop":
+    choice = input("Vyber možnosť: ")
+
+    if choice == "1":
+        movie = movie_input()
+        movies.append(movie)
+
+    elif choice == "2":
+        name = input("Zadaj názov filmu: ")
+        movie = get_movie_from_api(name)
+        if movie:
+            movies.append(movie)
+            print("Film pridaný z API!")
+        else:
+            print("Film sa nenašiel.")
+
+    elif choice == "3":
+        find_movie(movies)
+
+    elif choice == "4":
+        print("\nZoznam filmov:")
+        for m in movies:
+            m.show_info()
+
+    elif choice == "5":
         break
 
-    movie = movie_input()
-    movies.append(movie)
-
-print("\nZoznam filmov:")
-for m in movies:
-    m.show_info()
+    else:
+        print("Neplatná voľba.")
